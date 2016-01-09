70c2181a491dc50e371a0a0957a389cc0ad88e16
Rene Argento <rene.argento@gmail.com>
2016-01-09 16:59:53 -0200

    Added day 1

---
+++ Java Solutions/Day1.java
@@ -0,0 +1,16 @@
+
+public class Day1 {
+
+	public static void main(String[] args) {
+	        System.out.println("Primitive : double");
+	        System.out.println("Primitive : char");
+	        System.out.println("Primitive : boolean");
+	        System.out.println("Primitive : int");
+	        System.out.println("Referenced : String");
+	        System.out.println("Primitive : boolean");
+	        System.out.println("Primitive : double");
+	        System.out.println("Primitive : char");
+	        System.out.println("Referenced : String");
+	    }
+
+}
